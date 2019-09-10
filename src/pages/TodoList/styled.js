b72f7e4878baab0d4dc48a9b753bd4f607ae9251
Alison Earney <alie158@hotmail.com>
2019-09-10 13:38:09 +0100

    change font

--- src/pages/TodoList/styled.js
+++ src/pages/TodoList/styled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const TodoListTitle = styled.h1`
   color: ${({theme}) =>  theme.colours.title};
-  font-size: 30px;
+  font-size: 300px;
   text-transform: uppercase;
 `;
