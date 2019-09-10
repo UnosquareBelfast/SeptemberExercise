9e00583021469a5ea2708f2a7bc3fac9060f612d
Alison Earney <alie158@hotmail.com>
2019-09-10 13:38:57 +0100

    font

--- src/pages/TodoList/styled.js
+++ src/pages/TodoList/styled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const TodoListTitle = styled.h1`
   color: ${({theme}) =>  theme.colours.title};
-  font-size: 300px;
+  font-size: 30px;
   text-transform: uppercase;
 `;
