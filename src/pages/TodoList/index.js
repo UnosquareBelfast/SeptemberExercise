008db6410018d68574336d1403d87c1f7471dcac
Alison Earney <alie158@hotmail.com>
2019-09-11 15:17:01 +0100

    Update function added and some style changes

--- src/pages/TodoList/index.js
+++ src/pages/TodoList/index.js
@@ -1,34 +1,34 @@
 import React, { Fragment } from 'react';
 import { PropTypes as PT } from 'prop-types';
+import { TodoListItem } from './components';
 import container from './container';
 import { 
-  TodoListTitle
+  TodoListTitle, TitleContainer,TaskContainer,ToDoItem, ItemButtons, NewTask,AddTaskButton, TaskTextArea
  } from './styled';
 
-export const TodoList = ({ deleteItemFromList, isLoading, todoListItems }) => {
+export const TodoList = ({ deleteItemFromList, isLoading, todoListItems, updateItemOnList, createItemOnList,handleChange,handleTaskSubmission }) => {
 
-  const buildTodoListItems = (items) => {
-      return items && items.length > 0 ? (
-      <ul>
-        {items.map(x => 
-          (<li key={x.id}>
-            {x.title}
-            <button onClick={() => deleteItemFromList(x.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>) : 'Nothing to do.';
-  }
+  const buildTodoListItems = (items, deleteItem) => {
+    return (<ul><TaskContainer>{items.map(x => (<TodoListItem key={x.id} item={x} deleteItem={deleteItem} />))}</TaskContainer></ul>)
+  };
 
   return (
       <Fragment>
-        <TodoListTitle>Todo List</TodoListTitle>
-        {isLoading ? 'Loading...' : buildTodoListItems(todoListItems)}
+        <TodoListTitle>To Do List</TodoListTitle>
+        <NewTask>
+          <TaskTextArea><textarea id='newtasktitle'></textarea></TaskTextArea>
+          <AddTaskButton><button onClick={() => createItemOnList(document.getElementById('newtasktitle').value)}> Add Task </button></AddTaskButton>
+        </NewTask>
+        {isLoading ? 'Loading...' : buildTodoListItems(todoListItems, deleteItemFromList)}
       </Fragment>
   );
 };
 
+
 TodoList.propTypes = {
   deleteItemFromList: PT.func.isRequired,
+  createItemOnList: PT.func.isRequired,
+  updateItemOnList: PT.func.isRequired,
   isLoading: PT.bool.isRequired,
   todoListItems: PT.array.isRequired,
 };
