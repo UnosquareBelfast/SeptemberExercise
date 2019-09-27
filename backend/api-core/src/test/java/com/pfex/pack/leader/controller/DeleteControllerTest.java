5f892e1084995276f89d6063af2e2b2a1e2a1e4e
Toby <toby.bell@unosquare.com>
2019-09-27 09:54:43 +0100

    added get endpoint test, for all delelte todos valid

--- backend/api-core/src/test/java/com/pfex/pack/leader/controller/DeleteControllerTest.java
+++ backend/api-core/src/test/java/com/pfex/pack/leader/controller/DeleteControllerTest.java
@@ -2,6 +2,7 @@ package com.pfex.pack.leader.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 import com.pfex.pack.leader.model.DeletedTodos;
 import com.pfex.pack.leader.repository.DeletedTodoRepository;
@@ -10,12 +11,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
@@ -32,6 +36,24 @@ public class DeleteControllerTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(controller);
+        for (int i = 0; i < 3; i++) {
+            DeletedTodos todos = new DeletedTodos(i,i, "test " + i);
+            listOfDeletedTodos.add(todos);
+        }
+    }
+    @Test
+    public void whenGetAllTodosIsCalledAndIsValid() {
+        // Arrange
+        when(repository.findAll()).thenReturn(listOfDeletedTodos);
+
+        // Act
+        ResponseEntity response = controller.allDeletedTodos();
+
+        //Assert
+        assertEquals(listOfDeletedTodos,response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        System.out.println(response);
+        System.out.println(listOfDeletedTodos);
     }
 
 
