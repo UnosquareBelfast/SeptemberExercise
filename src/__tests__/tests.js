2bf9e43aa1ae53afb60fdb7021c870b90607b677
Toby <toby.bell@unosquare.com>
2019-10-01 14:56:21 +0100

    Added in example tests for front end from git

---
+++ src/__tests__/tests.js
@@ -0,0 +1,54 @@
+import instance from "../utilities/Axios";
+import MockAdapter from "axios-mock-adapter";
+
+import { retrieveTodoList, retrieveTodoListItem } from "../services/todoService";
+
+var mock = new MockAdapter(instance);
+
+afterEach(() => {
+  mock.reset();
+});
+
+describe("Todos", () => {
+  it("retrieveTodoList gets all todo items", done => {
+    const todoList = [
+      {
+        id: 1,
+        title: "My first note"
+      },
+      {
+        id: 2,
+        title: "My second note"
+      }
+    ];
+
+    mock.onGet("todos/").reply(200, todoList);
+
+    retrieveTodoList().then(res => {
+      // console.log(`res ${JSON.stringify(res)}`);
+      expect(res).toBe(todoList);
+      done();
+    });
+  });
+
+  it("retrieveTodoList gets item by id", done => {
+    const todoListItem =
+      {
+        id: 1,
+        title: "My first note"
+      };
+
+    mock.onGet("todos/1").reply(200, todoListItem);
+
+    retrieveTodoListItem(1).then(res => {
+      // console.log(`res ${JSON.stringify(res)}`);
+      expect(res).toStrictEqual(todoListItem);
+      done();
+    });
+  });
+});
+
+describe("DeletedTodos", () => {
+
+
+});
