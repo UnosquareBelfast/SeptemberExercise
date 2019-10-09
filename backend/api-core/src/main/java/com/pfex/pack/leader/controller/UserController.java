a9312648885685dcb94a2a52d215a1d4a41653d4
Toby <toby.bell@unosquare.com>
2019-10-09 09:15:37 +0100

    added user by id

--- backend/api-core/src/main/java/com/pfex/pack/leader/controller/UserController.java
+++ backend/api-core/src/main/java/com/pfex/pack/leader/controller/UserController.java
@@ -1,20 +1,27 @@
 package com.pfex.pack.leader.controller;
 
 import com.pfex.pack.leader.model.todos.Todos;
+import com.pfex.pack.leader.model.users.user;
+import com.pfex.pack.leader.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserController {
+    @Autowired
+    private UserRepository repository;
 
-    @GetMapping()
-    public ResponseEntity allTodos() {
-        List<Todos> response = repository.findAll();
-        if (response.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        } else {
-            return ResponseEntity.ok(response);
-        }
+
+    @GetMapping("{id}")
+    public ResponseEntity getTodosById(@PathVariable Integer id) {
+        Optional<user> response = repository.findById(id);
+
+        return response.isPresent() ?
+                ResponseEntity.ok(response.get()) :
+                ResponseEntity.notFound().build();
     }
 }
